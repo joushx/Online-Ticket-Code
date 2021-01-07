73ddcb0c76976cf78d37c5a36a66f6a948ad588e
Johannes Mittendorfer <johannes@johannes-mittendorfer.com>
2021-01-07 10:49:46 +0100

    Add example file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,11 @@
-wget https://railpublickey.uic.org/download.php -O uic-keys/keys.xml
+# Download keys
+mkdir -p uic-keys
+wget https://railpublickey.uic.org/download.php --no-check-certificate -O uic-keys/keys.xml
+
+# Generate model
 kaitai-struct-compiler ticket.ksy -t python
+
+# Setup python environment
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
